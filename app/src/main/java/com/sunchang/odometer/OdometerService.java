c6effaeff479e7557ead32eb33dbcce8d4fa3c41
Justin <justinsun@yeah.net>
2018-03-14 23:18:58 +0800

    First commit.

---
+++ app/src/main/java/com/sunchang/odometer/OdometerService.java
@@ -0,0 +1,69 @@
+package com.sunchang.odometer;
+
+import android.app.Service;
+import android.content.Intent;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Binder;
+import android.os.Bundle;
+import android.os.IBinder;
+
+public class OdometerService extends Service {
+
+    private IBinder binder = new OdometerBinder();
+
+    private Location lastLocation = null;
+
+    private double distanceInMeters;
+
+    private class OdometerBinder extends Binder {
+        OdometerService getOdometer() {
+            return OdometerService.this;
+        }
+    }
+
+    public OdometerService() {
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return this.binder;
+    }
+
+    @Override
+    public void onCreate() {
+        LocationListener listener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                if (lastLocation == null) {
+                    lastLocation = location;
+                }
+                distanceInMeters += location.distanceTo(lastLocation);
+                lastLocation = location;
+            }
+
+            @Override
+            public void onStatusChanged(String s, int i, Bundle bundle) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String s) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String s) {
+
+            }
+        };
+        LocationManager locationManager = (LocationManager) this.getSystemService(LOCATION_SERVICE);
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000,
+                1, listener);
+    }
+
+    private double getMiles() {
+        return this.distanceInMeters / 1609.344;
+    }
+}
